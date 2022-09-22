1fc45becc163eec4c595257557ba39bf8adfb356
RegisSalesRA <regismsales@gmail.com>
2022-09-22 15:41:03 -0300

    login page finished

---
+++ lib/ui/views/auth/sign_in_screen.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:nuapp/utils/colors_utils.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Container(
+          width: size.width,
+          height: size.height,
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 500,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text(
+                      "Let's you in",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30),
+                    ),
+                    SizedBox(
+                      height: 70,
+                      width: double.infinity,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          side: BorderSide(
+                            width: 2,
+                            color: Palette.lightGrey,
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Padding(
+                          padding: EdgeInsets.only(left: size.width / 4),
+                          child: Row(children: [
+                            Image.asset(
+                              'assets/images/facebook_icon.png',
+                              height: size.height * 0.05,
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              child: Text(
+                                'Facebook',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            )
+                          ]),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 70,
+                      width: double.infinity,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          side: BorderSide(
+                            width: 2,
+                            color: Palette.lightGrey,
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Padding(
+                          padding: EdgeInsets.only(left: size.width / 4),
+                          child: Row(children: [
+                            Image.asset(
+                              'assets/images/google_icon.png',
+                              height: size.height * 0.05,
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              child: Text(
+                                'Google',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            )
+                          ]),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 70,
+                      width: double.infinity,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          side: BorderSide(
+                            width: 2,
+                            color: Palette.lightGrey,
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Padding(
+                          padding: EdgeInsets.only(left: size.width / 4),
+                          child: Row(children: [
+                            Image.asset(
+                              'assets/images/apple_icon.png',
+                              height: size.height * 0.05,
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 15),
+                              child: Text(
+                                'Apple',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            )
+                          ]),
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      "Or",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 50,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Palette.primaryColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                        ),
+                        child: const Text("Sign in with password"),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Text.rich(
+                  TextSpan(
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Dont\'t have an account?',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey.shade700,
+                        ),
+                      ),
+                      const TextSpan(
+                        text: ' Sign up',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Palette.primaryColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/colors_utils.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class Palette {
+  static const Color primaryColor = Color(0xFFFe5871);
+  static Color textColor = Colors.black;
+  static Color lightGrey = Colors.grey.shade300;
+}
